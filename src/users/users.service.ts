cc673e51b8e66544f662df4c06bfcbec9908d2b3
Taras <tarasmikulskyi@ukr.net>
2023-05-10 14:54:53 +0300

    Database configuration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(
+		@InjectModel(User)
+		private userModel: typeof User,
+	) {}
+
+	findOne(filter: { where: { id?: string; username?: string; email?: string } }): Promise<User> {
+		return this.userModel.findOne({ ...filter });
+	}
+
+	async create(createUserDto: CreateUserDto): Promise<User | { warningMessage: string }> {
+		const user = new User();
+		const existingByUsername = await this.findOne({
+			where: { username: createUserDto.username },
+		});
+		const existingByEmail = await this.findOne({
+			where: { email: createUserDto.email },
+		});
+
+		if (existingByUsername) {
+			return { warningMessage: 'A user with this name already exists' };
+		} else if (existingByEmail) {
+			return { warningMessage: 'A user with this email already exists' };
+		}
+	}
+}
