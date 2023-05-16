f7e8edfe7f2015b93f2eecefcf9e0ad1ec82e458
Taras <tarasmikulskyi@ukr.net>
2023-05-16 11:24:31 +0300

    Dto validation created

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as passport from 'passport';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
 	);
 	app.use(passport.initialize());
 	app.use(passport.session());
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
 	const config = new DocumentBuilder()
 		.setTitle('Manga UA')
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
 
 export class CreateUserDto {
 	@ApiProperty({ example: 'CoolName' })
 	@IsNotEmpty()
+	@MinLength(5)
 	readonly username: string;
 
 	@ApiProperty({ example: 'cool_password' })
 	@IsNotEmpty()
+	@MinLength(8)
 	readonly password: string;
 
 	@ApiProperty({ example: 'cool@email.com' })
 	@IsNotEmpty()
+	@IsEmail()
 	readonly email: string;
 }
