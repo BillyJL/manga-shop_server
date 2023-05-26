e760d92181c8914928aef1b3221cb01127cf7421
Taras <tarasmikulskyi@ukr.net>
2023-05-26 12:26:12 +0300

    manga pagination and filtering

--- src/mangas/mangas.service.ts
+++ src/mangas/mangas.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Manga } from './mangas.model';
+import { IMangaQuery } from './types';
 
 @Injectable()
-export class MangasService {}
+export class MangasService {
+	constructor(@InjectModel(Manga) private mangaModel: typeof Manga) {}
+
+	async paginateAndFilter(query: IMangaQuery): Promise<{ count: number; rows: Manga[] }> {
+		const limit = +query.limit;
+		const offset = +query.offset * 20;
+		return this.mangaModel.findAndCountAll({
+			limit,
+			offset,
+		});
+	}
+}
---
+++ src/mangas/types/index.ts
@@ -0,0 +1,4 @@
+export interface IMangaQuery {
+	limit: string;
+	offset: string;
+}
