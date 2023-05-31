0a00bac90a05d96f10d3568e67300a2997b5bd9e
Taras <tarasmikulskyi@ukr.net>
2023-05-31 14:35:17 +0300

    creating a ShoppingCart model

---
+++ src/shopping-cart/shopping-cart.model.ts
@@ -0,0 +1,28 @@
+import { Table, Model, Column } from 'sequelize-typescript';
+
+@Table
+export class ShoppingCart extends Model {
+	@Column
+	userId: number;
+
+	@Column
+	mangaId: number;
+
+	@Column
+	name: string;
+
+	@Column
+	price: number;
+
+	@Column
+	image: string;
+
+	@Column({ defaultValue: 0 })
+	amount: number;
+
+	@Column
+	count: number;
+
+	@Column
+	total_price: number;
+}
