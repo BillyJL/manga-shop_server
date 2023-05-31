fc59159efe6ed196c2093d98ba49c149c31a27fb
Taras <tarasmikulskyi@ukr.net>
2023-05-31 19:03:16 +0300

    Method for adding manga to the cart

--- src/shopping-cart/shopping-cart.service.ts
+++ src/shopping-cart/shopping-cart.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { ShoppingCart } from './shopping-cart.model';
 import { UsersService } from 'src/users/users.service';
 import { MangasService } from 'src/mangas/mangas.service';
+import { AddToCartDto } from './dto/add-to-cart.dto';
 
 @Injectable()
 export class ShoppingCartService {
@@ -15,4 +16,22 @@ export class ShoppingCartService {
 	async findAll(userId: number | string): Promise<ShoppingCart[]> {
 		return this.shoppingCart.findAll({ where: { userId } });
 	}
+
+	async add(addToCartDto: AddToCartDto) {
+		const cart = new ShoppingCart();
+		const user = await this.userService.findOne({ where: { username: addToCartDto.username } });
+		const manga = await this.mangaService.findOne(addToCartDto.mangaId);
+
+		cart.userId = user.id;
+		cart.mangaId = manga.id;
+		cart.mangaName = manga.name;
+		cart.price = manga.price;
+		cart.image = JSON.stringify(manga.images)[0];
+		cart.amount = manga.amount;
+		cart.total_price = manga.price;
+
+		return cart.save();
+	}
+
+	
 }
