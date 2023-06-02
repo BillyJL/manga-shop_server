025502d15c05e62800684cd46a2b607530d5a3f9
Taras <tarasmikulskyi@ukr.net>
2023-06-02 11:11:29 +0300

    Fixing an error when importing a model instead of a module

--- src/shopping-cart/shopping-cart.module.ts
+++ src/shopping-cart/shopping-cart.module.ts
@@ -4,10 +4,10 @@ import { ShoppingCartService } from './shopping-cart.service';
 import { ShoppingCart } from './shopping-cart.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from 'src/users/users.module';
-import { Manga } from 'src/mangas/mangas.model';
+import { MangasModule } from 'src/mangas/mangas.module';
 
 @Module({
-	imports: [SequelizeModule.forFeature([ShoppingCart]), UsersModule, Manga],
+	imports: [SequelizeModule.forFeature([ShoppingCart]), UsersModule, MangasModule],
 	controllers: [ShoppingCartController],
 	providers: [ShoppingCartService],
 })
